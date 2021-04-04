72ec72901f6eac1385f0d41e07f292e50c9dcee6
binathi <binathiginnela@gmail.com>
2021-04-04 16:36:54 +0530

    day13

---
+++ Day-13/Emp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from Emp import views
+
+urlpatterns=[
+    path('',views.home,name="hm"),
+    path('abt/',views.about,name="ab"),
+    path('con/',views.contactus,name="co"),
+    path('lo/',views.login,name="log"),
+    path('reg/',views.register,name="re"),
+    path('cr/',views.crud,name="cd"),
+    path('del/<int:id>',views.deletedata,name="delete"),
+    path('df/',views.dform,name="dff"),
+]
